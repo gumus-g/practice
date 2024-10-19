f8abde8cdb568258bc4f52b7e8f68ef74f110669
Gulper Gumus <65042768+gumus-g@users.noreply.github.com>
2024-10-19 23:58:11 +0000

    GG Commit add loop file

---
+++ loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+   
+    for ( int i=1; i<=5; i++) {
+        printf("%s %d\n", "Go Beavers!!!", i);
+    }
+    return 0;
+
+    //i= 1;
+    //j= 1;
+    //while ((i+j)<=5){
+    //printf("%s %d\n", "Go Beavers!!!", i);
+    //j++;
+    //i++;
+    //}
+    //return 0;
+}
+
+
