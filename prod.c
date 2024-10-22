5233118c0dba1f098a1cecbf28f9c1b374a0c6bf
Gulper Gumus <65042768+gumus-g@users.noreply.github.com>
2024-10-22 22:08:04 +0000

    GG commit add more

--- prod.c
+++ prod.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-#define SIZE 5
+#include <stdlib.h>
+#define SIZE 100
+
+int* createArray(int n);
+void printArray(int *p, int n);
+int prodArray(int *p, int n);
 
 int main (int argc, char *argv[]) {
-    int myArray[SIZE];
+    int *myArray;
     int n;
-    int prod = 1;
+    int prod;
     char msg[200];
     int *p = NULL;
 
@@ -16,31 +21,44 @@ int main (int argc, char *argv[]) {
         printf ("Number of elements (%d) is invalid.\n",n);
         return 1;
     }
-
-    p = &myArray[0];
-    for (int i=0;i<n;i++) {
-        printf("Enter element %d of myArray: ",i);
-        if (scanf("%d",p) != 1) {
-            printf ("Error reading number.\n");
-            return 1;
-        }
-        p = p + 1;
-    }
-
-    p = &myArray[0];
-
+    myArray = createArray(n);
+    if (myArray != NULL) {
+        printArray(myArray, n);
+        prod = prodArray(myArray, n);
+        printf("The product of all elements of myArray is %d\n",prod);
+        free(myArray);
+        return 0;        
+    }   
+    printf("Error allocating memory. \n");
+    return 1;
+}
+int* createArray(int n) {
+    int *a = (int *) malloc(sizeof(*a)*n);
+        if (a != NULL) {
+            int *p = a;
+            for (int i=0;i<n;i++) {
+                printf("Enter element %d of myArray: ",i);
+                if (scanf("%d",p) != 1) {
+                    printf ("Error reading number.\n");
+                    free(a);
+                    return NULL;
+                }
+                p = p + 1;
+            }
+        return a;
+    }    
+}
+void printArray(int *p, int n) {
     for (int i=0; i<n; i++) {
         printf("myArray[%d] = %d\n",i,*p);
         p++;
     }
-
-    p = &myArray[0];
+}
+int prodArray(int *p, int n) {
+    int prod = 1;
     for (int i=0; i<n; i++) {
         prod = prod*(*p);
         p++; 
     }
-
-    printf("The product of all elements of myArray is %d\n",prod);
-    return 0;    
-
+    return prod;
 }
