66d3de4971ee416f6f2e324d5648ff4bc42246a6
Gulper Gumus <65042768+gumus-g@users.noreply.github.com>
2024-10-22 02:51:09 +0000

    GG commit add

--- prod.c
+++ prod.c
@@ -7,6 +7,7 @@ int main (int argc, char *argv[]) {
     int n;
     int prod = 1;
     char msg[200];
+    int *p = NULL;
 
     sprintf(msg, "how many elements in the array (1-%d)? ", SIZE);
     printf("%s", msg);
@@ -15,19 +16,30 @@ int main (int argc, char *argv[]) {
         printf ("Number of elements (%d) is invalid.\n",n);
         return 1;
     }
+
+    p = &myArray[0];
     for (int i=0;i<n;i++) {
         printf("Enter element %d of myArray: ",i);
-        if (scanf("%d",&myArray[i]) != 1) {
+        if (scanf("%d",p) != 1) {
             printf ("Error reading number.\n");
             return 1;
         }
+        p = p + 1;
     }
+
+    p = &myArray[0];
+
     for (int i=0; i<n; i++) {
-        printf("myArray[%d] = %d\n",i,myArray[i]);
+        printf("myArray[%d] = %d\n",i,*p);
+        p++;
     }
+
+    p = &myArray[0];
     for (int i=0; i<n; i++) {
-        prod *= myArray[i];
+        prod = prod*(*p);
+        p++; 
     }
+
     printf("The product of all elements of myArray is %d\n",prod);
     return 0;    
 
