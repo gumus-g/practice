87eb33a7f89905e9fda5d55ec0aaa64c6882cab2
Gulper Gumus <65042768+gumus-g@users.noreply.github.com>
2024-10-23 01:43:10 +0000

    GG commit adding

--- structures.c
+++ structures.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 int main (int argc, char *argv[]){
     struct PatientData {
         char name[20];  //patients's first name
         int age;        //Patient's age in years
         float weight;   //Patient's name in pounds
     };
+    typedef struct PatientData Patient;
 
-    struct PatientData tim;
+    Patient tim = {"Tim", 35, 150.0};
+    printf("Name:%s age:%d weight:%.2f\n", tim.name, tim.age, tim.weight);
+
+    strcpy(tim.name, "Timoty");
+    tim.age = 36;
+    tim.weight = 180.0;
+    printf("Name:%s age:%d weight:%.2f\n", tim.name, tim.age, tim.weight);
 }
