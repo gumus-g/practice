73c50b847fc4344eb9ab088db3b3eaa05f85a031
Gulper Gumus <65042768+gumus-g@users.noreply.github.com>
2024-10-23 00:19:20 +0000

    GG commit add file

---
+++ structures.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (int argc, char *argv[]){
+    struct PatientData {
+        char name[20];  //patients's first name
+        int age;        //Patient's age in years
+        float weight;   //Patient's name in pounds
+    };
+
+    struct PatientData tim;
+}
