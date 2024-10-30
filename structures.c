f6425a53029f6f9bdc62c5204ce777f88e05688a
Gulper Gumus <65042768+gumus-g@users.noreply.github.com>
2024-10-30 01:15:00 +0000

    GG commit add more detail

--- structures.c
+++ structures.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int main (int argc, char *argv[]){
     struct PatientData {
         char name[20];  //patients's first name
@@ -7,12 +8,47 @@ int main (int argc, char *argv[]){
         float weight;   //Patient's name in pounds
     };
     typedef struct PatientData Patient;
-
     Patient tim = {"Tim", 35, 150.0};
+    Patient class[3];
+    Patient *p;
+    Patient *pa;
+
     printf("Name:%s age:%d weight:%.2f\n", tim.name, tim.age, tim.weight);
 
     strcpy(tim.name, "Timoty");
     tim.age = 36;
     tim.weight = 180.0;
     printf("Name:%s age:%d weight:%.2f\n", tim.name, tim.age, tim.weight);
+    printf("\n");
+
+    strcpy(class[0].name, "John");
+    strcpy(class[1].name, "Jill");
+    strcpy(class[2].name, "Jack");
+
+    for (int i=0; i<3; i++) {
+        class[i].age = 10*i +10;
+        class[i].weight = 180 - 10*i;
+
+    }
+    for (int i=0; i<3; i++) {
+        printf("Name:%s age:%d weight:%.2f\n", class[i].name, class[i].age, class[i].weight);
+    }
+
+    p = (Patient *)calloc(3, sizeof(*p));
+
+    pa = p;
+    strcpy(pa->name, "Mary");
+    strcpy((pa+1)->name, "Mark");
+    strcpy((pa+2)->name, "Maya");
+
+    for (int i=0; i<3; i++) {
+        (pa+i)->age = 10*i +15;
+        (pa+i)->weight = 180 - 20*i;
+    }
+
+    for (int i=0; i<3; i++) {
+        printf("Name:%s age:%d weight:%.2f\n", (pa+i)->name, (pa+i)->age, (pa+i)->weight);
+    }
+    free(p);
+    return 0;
 }
